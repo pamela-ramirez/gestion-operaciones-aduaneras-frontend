dd736a1ed32d2b9851d456bcf7d06863395b2ab1
Pamela Ramirez Espinoza <pamela89.r@gmail.com>
2026-04-28 13:47:20 -0300

    feat(auth): implementar rutas protegidas con validacion token y mejora en flujo de autenticación

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
-import React from 'react';
-import Login from './pages/login/Login';
-import Home from './pages/home/Home';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
+import React from "react";
+import Login from "./pages/login/Login";
+import Home from "./pages/home/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import PrivateRoute from "./routes/PrivateRoute";
 
 export default function App() {
-    return (
+  return (
     <BrowserRouter>
       <Routes>
-
-        <Route path="/" element={ <Login/>} />
-        <Route path="/home" element={<Home />} />
-
+        <Route path="/" element={<Login />} />
+        <Route
+          path="/home"
+          element={
+            <PrivateRoute>
+              <Home />
+            </PrivateRoute>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );

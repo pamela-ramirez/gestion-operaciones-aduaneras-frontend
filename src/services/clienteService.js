549a423bd5058c7fa0cc7cc3c1e4d366566cd2d9
Pamela Ramirez Espinoza <pamela89.r@gmail.com>
2026-05-03 13:09:50 -0300

    refactor: limpiar código y renombrar constantes para mayor claridad

--- src/services/clienteService.js
+++ src/services/clienteService.js
@@ -1,6 +1,5 @@
 import apiClient from "../api/apiClient";
 
-// clienteService.js
 export const crearCliente = async (data) => {
   const respuesta = await apiClient.post(
     import.meta.env.VITE_CLIENTE_ENDPOINT,
--- src/services/despachanteService.js
+++ src/services/despachanteService.js
@@ -1,10 +1,9 @@
 import apiClient from "../api/apiClient";
 
-// despachanteService.js
 export const crearDespachante = async (data) => {
-  const res = await apiClient.post(
+  const respuesta = await apiClient.post(
     import.meta.env.VITE_DESPACHANTE_ENDPOINT,
     data
   );
-  return res.data;
+  return respuesta.data;
 };

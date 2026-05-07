f5c3843d0cc7b94be5ef5bca6d00e6f4560249eb
Pamela Ramirez Espinoza <pamela89.r@gmail.com>
2026-05-07 13:11:34 -0300

    feat(topbar): mostrar nombre de usuario logueado almacenado en el localstorage

--- src/layouts/Topbar.jsx
+++ src/layouts/Topbar.jsx
@@ -1,22 +1,25 @@
+import { obtenerUsuarioStorage } from "../utils/auth";
+
 export default function Topbar() {
+  const usuario = obtenerUsuarioStorage();
+  const nombreUsuario = usuario?.email?.split("@")[0] || "Usuario";
+
   return (
     <div className="topbar">
-
       <div className="topbar-search">
-       {/*  <i className="pi pi-search" />
+        {/*  <i className="pi pi-search" />
         <input placeholder="Buscar despacho o DUA..." /> */}
       </div>
 
       <div className="topbar-right">
         <i className="pi pi-bell"></i>
-      {/*   <i className="pi pi-cog"></i> */}
+        {/*   <i className="pi pi-cog"></i> */}
 
         <div className="user">
-          <span>Usuario</span>
+          <span>{nombreUsuario}</span>
           <div className="avatar" />
         </div>
       </div>
-
     </div>
   );
 }
